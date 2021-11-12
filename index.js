e22fbcb96459456053e2e1400cf849fffd363bde
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-11-12 19:33:30 +0600

    admin api added

--- index.js
+++ index.js
@@ -94,6 +94,17 @@ async function run() {
             res.json(result)
         });
 
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
         // Upsert Users to db
         app.put('/users', async (req, res) => {
             const user = req.body;
@@ -101,6 +112,14 @@ async function run() {
             const options = { upsert: true };
             const updateDoc = { $set: user };
             const result = await usersCollection.updateOne(filter, options, updateDoc);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
 
